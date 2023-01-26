b2b387908e5d09b5b06201444493124224cbbfd9
tim.bleimehl <tim.bleimehl@helmholtz-muenchen.de>
2023-01-26 13:44:20 +0100

    docs infra wip

--- make_docs.sh
+++ make_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p ./public
 cp README.md dict2graph_docs/index.md
-cp logo.png dict2graph_docs/logo.png
+#cp logo.png dict2graph_docs/logo.png
 
 mkdocs build --site-dir ./public --verbose
 cp dict2graph_docs/DOCS_README.md ./public/README.md
